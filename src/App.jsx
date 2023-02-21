15ee18ed305db27651059ff56c1755ca97a25902
loup esteban <esteban.loup@gmail.com>
2023-02-21 14:08:17 +0100

    homePage with login/connection button and text input for search

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,14 @@ import "./App.css";
 //import composant
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
-function App() {
+import HomePage from "./components/HomePage";
 
-	return (
-		<div className="flex flex-col min-h-screen">
-			<Header />
-			<Main />
-			<Footer />
-		</div>
-	);
-}
+const App = () => (
+	<div className="flex flex-col min-h-screen">
+		<Header />
+		<HomePage />
+		<Footer />
+	</div>
+);
 
 export default App;
--- src/components/Main/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Main = () => (
-	<div className="">
-
-	</div>
-
-);
-export default Main;
