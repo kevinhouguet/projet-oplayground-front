600b614e4653cb8c9dc9e990f7a182ee669bef30
loup esteban <esteban.loup@gmail.com>
2023-02-22 11:32:42 +0100

    :sparkles:feat: routes implementation + 404 page

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const HomePage = () => (
 	<div className="h-56 grid gap-10 content-center m-auto">
 		<div className="flex gap-8">
-			<button className="btn btn-primary py-4 px-8 flex items-center justify-center">Connexion</button>
-			<button className="btn btn-primary py-4 px-8 flex items-center justify-center">inscription</button>
+			<a href="/connexion">
+				<button className="btn btn-primary py-4 px-8 flex items-center justify-center">Connexion</button>
+			</a>
+			<a href="/inscription">
+				<button className="btn btn-primary py-4 px-8 flex items-center justify-center">inscription</button>
+			</a>
 		</div>
 		<div className="flex justify-around">
 			<input type="text" placeholder="Entrez votre adresse" className="input input-bordered input-secondary input-md w-full max-w-xl" />
