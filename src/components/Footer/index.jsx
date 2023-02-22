e35098d783efd48f96e98b188b1a9d684f8469bb
loup esteban <esteban.loup@gmail.com>
2023-02-22 21:36:45 +0100

    save

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import { FaTwitter, FaFacebook, FaGithub, FaInstagram  } from "react-icons/fa";
+
 const Footer = () => (
 	<footer className="footer bg-primary text-primary-content mt-auto p-5 flex justify-around">
 		<div>
-			<span className="footer-title text-3xl">O'playground</span>
+			<a href="/"><span className="footer-title text-3xl">O'playground</span></a>
 			<a href="/qui-sommes-nous" className="link link-hover">Qui sommes-nous ?</a>
 			<a className="link link-hover">Contact : admin@terrainsport.com</a>
 			<a className="link link-hover">Conditions générales</a>
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+//import backgroundImage from "../../assets/background.png"
 
 const HomePage = () => (
-	<div className="h-56 grid gap-10 content-center m-auto">
+	<div className="h-56 grid gap-10 content-center m-auto ">
 		<div className="flex gap-8">
 			<a href="/connexion">
 				<button className="btn btn-primary py-4 px-8 flex items-center justify-center">Connexion</button>
