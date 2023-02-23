920586652fd8530c0eafd7a595b66a3f8a01f64e
SaidMohamedDayas <113367982+SaidMohamedDayas@users.noreply.github.com>
2023-02-23 14:00:46 +0100

    :hammer: refactor: implementation email and password for login and inscription

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
+	const {handleSubmit, register, formState : {errors}} = useForm();
+
+	function onSubmit(data) {
+		console.log(data);
+	}
+
+	const [open, setOpen] = useState(false);
+	// handle toggle
+	const toggle = () => {
+		setOpen(!open);
+	};
 
 	return (
 		<div className="m-auto">
-	
+
 			<h1 className="flex justify-center p-6 text-2xl">Se connecter</h1>
+			
+			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-1 form-control w-full max-w-xs relative">
+			
+				<label className="label-text-xl"> Adresse mail* : </label>
+				<input className="input input-warning w-full max-w-xs" type="email" {...register("email", {required : true})} />
+				{errors.email && <p className="text-red-600 text-sm"> Adresse mail non renseignée </p>}
+
+				<label className="label-text-xl"> Mot de passe* : </label>
+				<input className="input input-warning w-full max-w-xs" type={(open === false)? "password" : "text"} 
+					{...register("password", {required : true, minLength : 8})}  />
+				{errors.password && <p className="text-red-600 text-sm"> Mot de passe obligatoire et doit être supérieur 8 caractères</p>}
 				
-			<form className="flex flex-col gap-1 form-control w-full max-w-xs">
-				<label className="label-text-xl"> Adresse mail : </label>
-				<input className="input input-warning w-full max-w-xs" id="adressemail" type="text"/>
-	
-				<label className="label-text-xl" htmlFor="password"> Mot de passe : </label>
-				<div className="relative w-full">
-					<div className="absolute inset-y-0 right-0 flex items-center px-2">
-						<input className="hidden js-password-toggle" id="toggle" type="checkbox" />
-					</div>
-					<input className="input input-warning w-full max-w-xs js-password" id="password" type="password"/>
+				<div className="text-2xl absolute bottom-20 right-3">
+					{
+						(open === false) ? <AiFillEyeInvisible onClick={toggle}/> : <AiFillEye onClick={toggle}/>
+					}
 				</div>
+
+				<div className="flex justify-center">
+					<button className="btn btn-primary my-7 " type="submit"> Connexion </button>
+				</div>
+
 			</form>
-			<div className="flex justify-center">
-				<button className="btn btn-primary my-7 " type="button"> Connexion </button>
-			</div>
-	
 		</div>
 	);
 };
