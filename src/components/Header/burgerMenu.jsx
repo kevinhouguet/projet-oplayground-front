f00223d0d0769a1d3aa7d45eb60ab462be9f42bb
loup esteban <esteban.loup@gmail.com>
2023-02-27 22:24:57 +0100

    Add favicon Oplayground

--- src/components/Header/burgerMenu.jsx
+++ src/components/Header/burgerMenu.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const BurgerMenu = () => {
-  <div className="burgerMenu dropdown dropdown-end bg-secondary rounded-xl">
-    <label tabIndex={0} className="btn btn-ghost rounded-btn">Bonjour Jean	&#127936;</label>
-      <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-secondary-focus rounded-box w-52 mt-4">
-        <li><a href="/mon-profil">Mon compte</a></li> 
-        <li><a>Mes événements</a></li>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="burgerMenu dropdown dropdown-end bg-secondary rounded-xl">
+      <label
+        tabIndex={0}
+        className="btn btn-ghost rounded-btn"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Bonjour Jean	&#127936;
+      </label>
+      <ul
+        tabIndex={0}
+        className={`menu dropdown-content p-2 shadow bg-secondary-focus rounded-box w-52 mt-4 ${isOpen ? "block" : "hidden"
+          }`}
+      >
+        <li>
+          <Link to="/mon-profil" onClick={handleClick}>
+            Mon compte
+          </Link>
+        </li>
+        <li>
+          <a onClick={handleClick}>Mes événements</a>
+        </li>
       </ul>
-  </div>
-}
+    </div>
+  );
+};
 
 export default BurgerMenu;
