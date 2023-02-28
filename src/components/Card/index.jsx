ab609fd2ef48fc436bd0945aa78a2f3f09189f90
gaelamand <gael.amand@gmail.com>
2023-02-28 14:28:12 +0100

    :sparkles: feat:Api geo and map with card componant

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,53 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
 import basketball from "../../assets/basketball.jpg";
-// import Ligneshorizontales from "../../assets/Ligneshorizontales.jpg";
+import { useLocation } from "react-router-dom";
 
-const Header = () => (
-	
-	<div className="min-h-screen bg-cover bg-center flex flex-wrap justify-center bg-[url('./assets/white-painted-wall-texture-background.jpg')] p-10">
-		<div className="card lg:card-side bg-base-100 shadow-xl w-9/12 m-10">
-			<img src={basketball} className="rounded-2xl w-1/3 h-110" alt="Album"/>
-			<div className="card-body">
-				<h2 className="card-title">SALLE MULTISPORTS DE PONT-DE-L'ISERE</h2>
-				<p className="mt-8 text-xl ">Surface : Gymnase</p>
-				<p className="text-xl">Adresse : Avenue du canal</p>
-				<p className="text-xl">26600 PONT DE L'ISERE</p>
-				<div className="card-actions justify-end">
-					<a href="/detail-terrain">
-						<button className="btn btn-primary py-2 px-4 text-lg">Plus d'information</button> 
-					</a>
-				</div>
-			</div>
-		</div>
 
-		<div className="card lg:card-side bg-base-100 shadow-xl w-9/12 m-10">
-			<img src={basketball} className="rounded-2xl w-1/3 h-110" alt="Album"/>
-			<div className="card-body">
-				<h2 className="card-title mt-3">SALLE MULTISPORTS DE PONT-DE-L'ISERE</h2>
-				<p className="mt-8 text-xl ">Surface : Gymnase</p>
-				<p className="text-xl">Adresse : Avenue du canal</p>
-				<p className="text-xl">26600 PONT DE L'ISERE</p>
-				<div className="card-actions justify-end">
-					<button className="btn btn-primary py-2 px-4 text-lg">Plus d'information</button>
-				</div>
-			</div>
-		</div>
+const Header = () => {
 
-		<div className="card lg:card-side bg-base-100 shadow-xl w-9/12 m-10">
-			<img src={basketball} className="rounded-2xl w-1/3 h-110" alt="Album"/>
-			<div className="card-body">
-				<h2 className="card-title mt-3">SALLE MULTISPORTS DE PONT-DE-L'ISERE</h2>
-				<p className="mt-8 text-xl ">Surface : Gymnase</p>
-				<p className="text-xl">Adresse : Avenue du canal</p>
-				<p className="text-xl">26600 PONT DE L'ISERE</p>
-				<div className="card-actions justify-end">
-					<button className="btn btn-primary py-2 px-4 text-lg">Plus d'information</button>
-				</div>
-			</div>
-		</div>
+  const location = useLocation();
+  const data = location.state;
+  console.log(data)
 
-		
-	</div>
 
-);
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://oplaygroundapi.herokuapp.com/api/terrains/26250")
+//       .then((response) => setData(response.data))
+//       .catch((error) => console.log(error));
+//   }, []);
+
+  return (
+    <div className="min-h-screen bg-cover bg-center flex flex-wrap justify-center bg-[url('./assets/white-painted-wall-texture-background.jpg')] p-10">
+      {data.map((item) => (
+        <div className="card lg:card-side bg-base-100 shadow-xl w-9/12 m-10">
+          <img src={basketball} className="rounded-2xl w-1/3 h-110" alt="Album" />
+          <div className="card-body">
+            <h2 className="card-title">{item.name}</h2>
+            <p className="mt-8 text-xl ">Surface : {item.surface}</p>
+            <p className="mt-8 text-xl ">Type : {item.type}</p>
+            <p className="text-xl">Adresse : {item.address}</p>
+            <p className="text-xl">{item.zipCode} {item.city}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary py-2 px-4 text-lg">Plus d'information</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Header;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logoOP from "../../assets/logoOP.png";
+import { Link } from "react-router-dom";
 
 const Header = () => (
 	<header className="navbar bg-primary text-primary-content p-5">
-		<a href="/">
+		<Link to="/">
 			<img src= {logoOP}  className="btn btn-ghost normal-case text-xl pointer-events-none" alt="Logo oplayground" />
-		</a>
+		</Link>
 
 		{/* <button className="btn btn-active">Button</button> */}
 	</header>
