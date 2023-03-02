4842e7bf9b4d0a5bf717f5dbd9417896dd4f2edf
loup esteban <esteban.loup@gmail.com>
2023-03-02 10:57:11 +0100

    :sparkles: :add creation confirmation modal after successfull connection

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,19 +2,20 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { bool, func, string } from "prop-types";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
 
 const Login = (props) => {
 	const [isError, setIsError] = useState(false);
-	
-	const { open, toggle, onLogout, token, setToken, username, setUsername } = props;
 
-	const { 
-		handleSubmit, 
+	const { open, toggle, token, setToken, username, setUsername } = props;
+
+	const {
+		handleSubmit,
 		register,
-		formState : {errors},
+		formState: { errors },
 	} = useForm();
 
 	const onSubmit = (data) => {
@@ -36,45 +37,70 @@ const Login = (props) => {
 		<div className="m-auto">
 
 			<h1 className="flex justify-center p-6 text-2xl">Se connecter</h1>
-			
+
 			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-1 form-control w-full max-w-xs relative">
-			
+
 				<label className="label-text-xl"> Adresse mail* : </label>
-				<input className="input input-warning w-full max-w-xs" type="email" {...register("email", {required : true})} />
+				<input className="input input-warning w-full max-w-xs" type="email" {...register("email", { required: true })} />
 				{errors.email && <p className="text-red-600 text-sm"> Adresse mail non renseignée </p>}
 
 				<label className="label-text-xl"> Mot de passe* : </label>
-				<input className="input input-warning w-full max-w-xs" type={(open === false) ? "password" : "text"} 
-					{...register("password", {required : true, minLength : 8})}  />
+				<input className="input input-warning w-full max-w-xs" type={(open === false) ? "password" : "text"}
+					{...register("password", { required: true, minLength: 8 })} />
 				{errors.password && <p className="text-red-600 text-sm"> Mot de passe obligatoire et doit être supérieur 8 caractères</p>}
-				
+
 				<div className="text-2xl absolute bottom-20 right-3">
 					{
-						(open === false) 
-						?
-						<AiFillEyeInvisible onClick={toggle}/> 
-						: 
-						<AiFillEye onClick={toggle}/>
+						(open === false)
+							?
+							<AiFillEyeInvisible onClick={toggle} />
+							:
+							<AiFillEye onClick={toggle} />
 					}
 				</div>
 
 				<p className="text-sm">*Champs obligatoires</p>
 
 				<div className="flex justify-center">
-					<button className="btn btn-primary my-7 " type="submit"> Connexion </button>
+					<label htmlFor="my-modal" className="btn" type="submit">
+						Connexion
+					</label>
+					<input
+						checked={username ? true : false}
+						type="checkbox"
+						id="my-modal"
+						className="modal-toggle"
+						form="login-form"
+						onChange={() => {}}
+					/>
+					<div className="modal">
+						<div className="modal-box">
+							<h3 className="font-bold text-lg">Hey {username} ca fait plaisir de te voir</h3>
+							<p className="py-4">
+								You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+							</p>
+							<div className="modal-action">
+								<Link to="/">
+									<label htmlFor="my-modal" className="btn">
+										C'est parti !
+									</label>
+								</Link>
+							</div>
+						</div>
+					</div>
 				</div>
-			</form>
+			</form >
 
 			{
 				token
-					&&
-					<p> Bonjour {username}. Connexion réussi ! </p>
+				&&
+				<p> Bonjour {username}. Connexion réussi ! </p>
 			}
 
 			{
 				isError
-					&&	
-					<p className="text-red-600 text-sm"> Connexion échouée ! Adresse mail et/ou mot de passe incorrect ! </p>
+				&&
+				<p className="text-red-600 text-sm"> Connexion échouée ! Adresse mail et/ou mot de passe incorrect ! </p>
 			}
 
 		</div>
